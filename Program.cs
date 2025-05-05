4b34cdb0998ae7aedd9ab47067dcb8d5a459d93d
emolesky <emilynnmolesky@gmail.com>
2025-05-04 19:47:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ballons
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "loonbalxballpoon";
+
+            Solution solution = new Solution();
+            int result = solution.MaxNumberOfBaloons(text);
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,44 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ballons
+{
+    public class Solution
+    {
+        public int MaxNumberOfBaloons(string text)
+        {
+            int bCount = 0;
+            int aCount = 0;
+            int lCount = 0;
+            int oCount = 0;
+            int nCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (c == 'b') bCount++;
+                else if (c == 'a') aCount++;
+                else if (c == 'l') lCount++;
+                else if (c == 'o') oCount++;
+                else if (c == 'n') nCount++;
+            }
+
+            lCount /= 2;
+            oCount /= 2;
+
+            int balloon = bCount;
+            if (aCount < balloon) balloon = aCount;
+            if (lCount < balloon) balloon = lCount;
+            if (oCount < balloon) balloon = oCount;
+            if (nCount < balloon) balloon = nCount;
+
+            return balloon;
+
+
+        }
+    }
+}
